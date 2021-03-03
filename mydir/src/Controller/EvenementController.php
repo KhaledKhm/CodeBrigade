c3804d4818095559bdf1f5cdea5b581b5cbcc426
djotayaraa <78907403+djotayaraa@users.noreply.github.com>
2021-03-03 22:14:21 +0100

    update

---
+++ mydir/src/Controller/EvenementController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Evenement;
+use App\Entity\Promotion;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class EvenementController extends AbstractController
+
+
+{
+    /**
+     * @Route("/evenement", name="evenement")
+     */
+    public function index(): Response
+    {
+        $class = $this->getDoctrine()->getRepository(Evenement::class)->findAll();
+
+        return $this->render('evenement/list.html.twig', ['classe'=>$class]);
+
+    }
+    /**
+     * @Route("/ajouterEvenement", name="ajouterEvenement")
+     */
+    public function createEvenement(Request $request)
+    {
+        if ($request->request->count()>0)
+        {
+            $evenement=new Evenement();
+            $evenement->setLibelle($request->get('Libelle'));
+            $evenement->setDescription($request->get('description'));
+            $evenement->setDateDebut($request->get('DateDebut'));
+            $evenement->setDateFin($request->get('DateFin'));
+            $evenement->setPrixInscription($request->get('PrixInscription'));
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($evenement);
+            $em->flush();
+            return $this->redirectToRoute('evenement');
+        }
+        return $this->render('evenement/add.html.twig');
+    }
+
+    /**
+     * @Route("/modifierEvenement/{id}", name="modifierEvenement")
+     */
+    public function updateEvenement(Request $request,$id)
+    {
+        if ($request->request->count()>0)
+        {
+            $em=$this->getDoctrine()->getManager();
+            $evenement=$em->getRepository(Evenement::class)->find($id);
+            $evenement->setLibelle($request->get('Libelle'));
+            $evenement->setDescription($request->get('description'));
+            $evenement->setDateDebut($request->get('DateDebut'));
+            $evenement->setDateFin($request->get('DateFin'));
+            $evenement->setPrixInscription($request->get('PrixInscription'));
+            $em->flush();
+            return $this->redirectToRoute('evenement');
+        }
+        $evenement=$this->getDoctrine()->getRepository(Evenement::class)->find($id);
+        return $this->render('evenement/update.html.twig', ['evenement'=>$evenement]);
+    }
+
+    /**
+     * @Route("/supprimerEvenement/{id}",name="supprimerEvenement")
+     */
+    public function deleteEvenement($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $class = $this->getDoctrine()->getRepository(Evenement::class)->find($id);
+        $em->remove($class);
+        $em->flush();
+        return $this->redirectToRoute('evenement');
+    }
+
+}
---
+++ mydir/src/Entity/Evenement.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EvenementRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EvenementRepository::class)
+ */
+class Evenement
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $libelle;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $DateDebut;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $DateFin;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $PrixInscription;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Promotion::class, inversedBy="evenements")
+     */
+    private $promotions;
+
+    public function __construct()
+    {
+        $this->promotions = new ArrayCollection();
+    }
+
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?string
+    {
+        return $this->DateDebut;
+    }
+
+    public function setDateDebut(string $DateDebut): self
+    {
+        $this->DateDebut = $DateDebut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?string
+    {
+        return $this->DateFin;
+    }
+
+    public function setDateFin(string $DateFin): self
+    {
+        $this->DateFin = $DateFin;
+
+        return $this;
+    }
+
+    public function getPrixInscription(): ?int
+    {
+        return $this->PrixInscription;
+    }
+
+    public function setPrixInscription(int $PrixInscription): self
+    {
+        $this->PrixInscription = $PrixInscription;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Promotion[]
+     */
+    public function getPromotions(): Collection
+    {
+        return $this->promotions;
+    }
+
+    public function addPromotion(Promotion $promotion): self
+    {
+        if (!$this->promotions->contains($promotion)) {
+            $this->promotions[] = $promotion;
+        }
+
+        return $this;
+    }
+
+    public function removePromotion(Promotion $promotion): self
+    {
+        $this->promotions->removeElement($promotion);
+
+        return $this;
+    }
+}
