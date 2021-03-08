9932f3b9ffad99a553b34988844c2d680f0c756b
dhaferslimi23 <47641213+dhaferslimi23@users.noreply.github.com>
2021-03-08 21:26:25 +0000

    update

---
+++ jobbook/src/Controller/AvisController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Avis;
+use App\Form\AvisType;
+use App\Repository\AvisRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+class AvisController extends AbstractController
+{
+    /**
+     * @Route("/avisss", name="aviss")
+     */
+    public function index(): Response
+    {
+        return $this->render('ajoutavis.html.twig', [
+            'controller_name' => 'AvisController',
+        ]);
+    }
+    /**
+     * @param AvisRepository $repository
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/Affiche",name="afficheravis")
+     */
+    public function Affiche(AvisRepository $repository)
+    {
+        $avis=$repository->findAll();
+        return $this->render('afficheavis.html.twig',
+            ['avis'=>$avis]);
+    }
+
+    /**
+     * @Route("/SupprimerAvis/{id}",name="deleteavis")
+     */
+    function Delete($id,AvisRepository $repository)
+    {
+        $avis=$repository->find($id);
+        $em=$this->getDoctrine()->getManager();
+        $em->remove($avis);
+        $em->flush();
+        return $this->redirectToRoute('afficheravis');
+    }
+
+
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route ("/Ajout",name="ajouteravis")
+     */
+    function Add(Request $request)
+    {
+        $avis=new Avis();
+        $form=$this->createForm(AvisType::class, $avis);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($avis);
+            $em->flush();
+            return $this->redirectToRoute('afficheravis');
+        }
+        return $this->render('ajoutavis.html.twig',
+            [
+                'form'=>$form->createView(),
+            ]
+        );
+    }
+
+
+    /**
+     * @param Request $request
+     * @Route("/Modifieravis/{id}",name="modifieravis")
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
+    function modifier(avisRepository $repository,$id,Request $request)
+    {
+        $avis=$repository->find($id);
+        $form=$this->createForm(AvisType::class,$avis);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em=$this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute('afficheravis');
+        }
+        return $this->render('ajoutavis.html.twig',
+            [
+                'form'=>$form->createView(),
+            ]
+        );
+    }
+
+
+}
---
+++ jobbook/src/Entity/Reclamation.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReclamationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ReclamationRepository::class)
+ */
+class Reclamation
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $subject;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $commentaire;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire(string $commentaire): self
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+}
