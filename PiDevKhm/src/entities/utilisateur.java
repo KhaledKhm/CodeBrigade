69a7858128e0954ebe0d4fcb80c996e9a972b618
KhaledKhm <47531479+KhaledKhm@users.noreply.github.com>
2021-04-11 23:45:29 +0100

    Update utilisateur.java

--- PiDevKhm/src/entities/utilisateur.java
+++ PiDevKhm/src/entities/utilisateur.java
@@ -5,10 +5,31 @@
  */
 package entities;
 
+import java.sql.Date;
+
 /**
  *
  * @author SPIRIT
  */
 public class utilisateur {
+    private int id;
+    private String password;
+    private String account_status;
+    private int cin_personne;
+    private String immatricule_entreprise;
+    private String nom_personne;
+    private String prenom_personne;
+    private String libelle_entreprise;
+    private String sexe_personne;
+    private Date date_nais_personne;
+    private String etat_sociale_personne;
+    private int telephone;
+    private String adresse;
+    private String email;
+    private String siteweb;
+    private String domaine_personne;
+    private String secteur_entreprise;
+    private String role;
+
     
 }
