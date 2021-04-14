863604ad39fd886700f9e507ff5f8b6d300d3b69
KhaledKhm <47531479+KhaledKhm@users.noreply.github.com>
2021-04-14 17:06:00 +0100

    utilisateur services

--- PiDevKhm/src/services/utilisateurService.java
+++ PiDevKhm/src/services/utilisateurService.java
@@ -5,7 +5,10 @@
  */
 package services;
 
+import entities.*;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import tools.MaConnexion;
 
 /**
@@ -20,5 +23,42 @@ public class utilisateurService {
         cnx = MaConnexion.getInstance().getCnx();
     }
     
+       public void ajouterUtilisateur(utilisateur p)
+   {
+        try {
+            String sql="insert into utilisateur(id,password)"+"Values(?,?)";
+            ste = cnx.prepareStatement(sql);
+            ste.setString(1,p.getPassword());
+            ste.setString(2, p.getAccount_status());
+            ste.executeUpdate();
+            System.out.println("Utilisateur Ajoutée");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+       
+   }
+       
+       
+      public List<utilisateur> afficherUtilisateurs(){
+       List<utilisateur> utilisateurs = new ArrayList<>();
+        try {
+            
+            String sql="select * from utilisateur";
+            ste =cnx.prepareStatement(sql);
+            ResultSet rs = ste.executeQuery();
+            while (rs.next()) { 
+                utilisateur u = new utilisateur();
+                u.setId(rs.getInt("id"));
+                u.setNom_personne(rs.getString(2));
+                u.setPrenom_personne(rs.getString("prenom"));
+                utilisateurs.add(u);
+                
+            
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+     return utilisateurs;
+   }
     
 }
