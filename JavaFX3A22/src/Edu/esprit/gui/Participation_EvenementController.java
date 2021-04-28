1d4d0790b569e49bd1e67f9822589007041c5272
djotayaraa <78907403+djotayaraa@users.noreply.github.com>
2021-04-29 00:51:32 +0100

    update

---
+++ JavaFX3A22/src/Edu/esprit/gui/Participation_EvenementController.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Edu.esprit.gui;
+
+import Edu.esprit.entities.Evenement;
+import Edu.esprit.entities.Participation_Evenement;
+import Edu.esprit.services.Participation_EvenementService;
+import java.io.IOException;
+import static java.lang.Integer.parseInt;
+import java.net.URL;
+import java.util.List;
+import java.util.Properties;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+/**
+ * FXML Controller class
+ *
+ * @author OneEffect007
+ */
+public class Participation_EvenementController implements Initializable {
+
+    @FXML
+    private TableView<Participation_Evenement> table_participation;
+    @FXML
+    private TableColumn<?, ?> col_participant_participation;
+    @FXML
+    private TableColumn<?, ?> col_eval_participation;
+    @FXML
+    private TextField tf_supp_id_participation;
+    @FXML
+    private Button btn_supp_participation;
+ ObservableList<Participation_Evenement> observableList;
+    @FXML
+    private Button page_evenements;
+    @FXML
+    private TableColumn<?, ?> tf_email_evenement;
+  
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        Rebelotte();
+    }
+
+    public void Rebelotte(){
+        //affichage tableau
+       
+        
+        Participation_EvenementService ps=new Participation_EvenementService();
+        List<Participation_Evenement> participations=ps.afficherParticipation();
+        
+ 
+        col_participant_participation.setCellValueFactory(new PropertyValueFactory<>("id_p"));
+        col_eval_participation.setCellValueFactory(new PropertyValueFactory<>("id_e"));
+        tf_email_evenement.setCellValueFactory(new PropertyValueFactory<>("email"));
+        
+       observableList = FXCollections.observableArrayList(participations);
+        table_participation.setItems(observableList);
+ 
+    }    
+
+    @FXML
+    private void supprimerParticipation(ActionEvent event) {
+            String id= (String) tf_supp_id_participation.getText();
+        Participation_EvenementService es=new Participation_EvenementService();
+        es.supprimerParticipation(id);
+        //alerte de suppression
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Nouveau Mise a Jour dans la base de données!");
+        alert.setHeaderText(null);
+        alert.setContentText("Participation Supprimée");
+        alert.showAndWait();
+        //refresh
+        Rebelotte();
+    
+    }
+    
+     @FXML
+    private void pageEvenements(ActionEvent event) {
+           try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Evenement.fxml"));
+            Parent root = loader.load();
+            EvenementController ec = loader.getController();
+            page_evenements.getScene().setRoot(root);
+            
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } 
+    }
+
+    
+    @FXML
+    private void click(MouseEvent event) {
+          Participation_Evenement selected = table_participation.getSelectionModel().getSelectedItem();
+        tf_email_evenement.setText(selected.getEmail() + "");
+    }
+
+    @FXML
+    private void Envoyeremail(ActionEvent event) throws AddressException, MessagingException {
+            //mailing
+             //String host = "relay.jangosmtp.net";
+              //props.put("mail.smtp.connectiontimeout", "t1");
+    //props.put("mail.smtp.timeout", "t2");
+
+        Properties properties = new Properties();
+
+        properties.put("mail.smtp.auth","true");
+        properties.put("mail.smtp.starttls.enable","true");
+        properties.put("mail.smtp.host","smtp.gmail.com");
+        properties.put("mail.smtp.starttls.port","587");
+
+        String MyAccountEmail= "djo90210kladjo@gmail.com";
+        String Password= "S90210kladjoa";
+
+        Session session;
+        session = Session.getDefaultInstance(properties,new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication(){
+                return new PasswordAuthentication(MyAccountEmail,Password);
+            }
+         
+        });
+       
+ Participation_EvenementService es=new Participation_EvenementService();
+ 
+          Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(MyAccountEmail));
+
+        message.setRecipient(Message.RecipientType.TO,new InternetAddress(es.getMail()));
+          
+       //String to = "mohamedjameleddine.kalai@esprit.tn";
+      // message.setRecipients(Message.RecipientType.TO,
+            //  InternetAddress.parse(to)); 
+        
+        message.setSubject("Nouveau participation a un Evenement");
+        message.setText("Monsieur/Madame,Nous sommes ravi de vous faire connaitre que vous etes participer a un Evenement");
+        
+        Transport.send(message);
+      
+        System.out.println("Sent message successfully....");
+    }
+
+    
+}
---
+++ JavaFX3A22/src/Edu/esprit/gui/PromotionController.java
@@ -0,0 +1,307 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Edu.esprit.gui;
+
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
+import Edu.esprit.entities.Evenement;
+import Edu.esprit.services.ServiceEvenement;
+import java.io.IOException;
+import static java.lang.Integer.parseInt;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.net.URL;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import Edu.esprit.entities.Evenement;
+import Edu.esprit.entities.Promotion;
+import Edu.esprit.services.ServicePromotion;
+import Edu.esprit.services.ServiceEvenement;
+import java.net.URL;
+import java.sql.SQLException;
+import static java.util.Collections.list;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import static javafx.collections.FXCollections.observableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author 21652
+ */
+public class PromotionController implements Initializable {
+
+    @FXML
+    private TableView<Promotion> table_promotion;
+    @FXML
+    private TableColumn<Promotion, Integer> col_id_entt;
+    @FXML
+    private TableColumn<Promotion, String> col_libelle_entt;
+    @FXML
+    private TableColumn<Promotion, Integer> col_PoucentageReduction_ent;
+    @FXML
+    private TableColumn<Promotion, Integer> col_id_evenement_ent;
+    @FXML
+    private TextField filterField;
+    @FXML
+    private TextField tf_ajout_libelle_promotion;
+    @FXML
+    private TextField tf_ajout_PoucentageReduction_promotion;
+    @FXML
+    private TextField tf_ajout_id_evenement_promotion;
+    @FXML
+    private TextField tf_modif_libelle_promotion;
+    @FXML
+    private TextField tf_modif_PoucentageReduction_promotion;
+    @FXML
+    private TextField tf_modif_id_evenement_promotion;
+    @FXML
+    private TextField tf_modif_id_promotion;
+
+    /**
+     * Initializes the controller class.
+     */
+    ObservableList<Promotion> observableList;
+    @FXML
+    private Button page_evenements;
+    @FXML
+    private Button btn_modif_promotion;
+    @FXML
+    private AnchorPane SmallPane;
+    @FXML
+    private Button btn_supp_promotion;
+    @FXML
+    private Button btn_ajout_promotion;
+
+    
+      @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        try {
+            Rebelottee();
+        } catch (SQLException ex) {
+            Logger.getLogger(PromotionController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+     public void Rebelottee() throws SQLException {
+        //affichage tableau
+        ServicePromotion es = new ServicePromotion();
+        List<Promotion> list = es.afficherPromotions();
+
+        col_id_entt.setCellValueFactory(new PropertyValueFactory<Promotion, Integer>("id"));
+        col_libelle_entt.setCellValueFactory(new PropertyValueFactory<Promotion, String>("libelle"));
+        col_PoucentageReduction_ent.setCellValueFactory(new PropertyValueFactory<Promotion, Integer>("PoucentageReduction"));
+        col_id_evenement_ent.setCellValueFactory(new PropertyValueFactory<Promotion, Integer>("id_evenement"));
+        
+   
+        ObservableList<Promotion> observableList = FXCollections.observableArrayList(list);
+       table_promotion.setItems(observableList);
+
+    }
+
+    @FXML
+    private void ajouterPromotion(ActionEvent event) throws MessagingException, SQLException {
+        //ajout
+        String libelle = tf_ajout_libelle_promotion.getText();
+        String PoucentageReduction = tf_ajout_PoucentageReduction_promotion.getText();
+        String id_evenement = tf_ajout_id_evenement_promotion.getText();
+       
+
+        Promotion p = new Promotion(libelle, parseInt(PoucentageReduction), parseInt(id_evenement));
+        ServicePromotion es = new ServicePromotion();
+
+        //controle de saisie
+        if (tf_ajout_libelle_promotion.getText().length()==0 ) {
+          tf_ajout_libelle_promotion.setStyle(" -fx-border-color: red ; -fx-border-width: 2px ;");
+          new animatefx.animation.Shake(tf_ajout_libelle_promotion).play();  
+        }
+       else 
+           tf_ajout_libelle_promotion.setStyle(null);
+        
+                   
+        if (tf_ajout_PoucentageReduction_promotion.getText().toString().length()==0 ) {
+          tf_ajout_PoucentageReduction_promotion.setStyle("-fx-border-color: red ; -fx-border-width: 2px ;");
+          new animatefx.animation.Shake(tf_ajout_PoucentageReduction_promotion).play();
+        }
+        else 
+           tf_ajout_PoucentageReduction_promotion.setStyle(null);
+        
+          //controle de saisie
+        boolean cond = true;
+        if (tf_ajout_libelle_promotion.getText().isEmpty() | tf_ajout_PoucentageReduction_promotion.getText().toString().isEmpty() ) {
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setTitle("Erreur");
+            alert.setHeaderText(null);
+            alert.setContentText("Vérifiez vos Champs!");
+            alert.showAndWait();
+            cond = false;
+        }
+        if (cond == true) {
+            es.ajouterPromotion(p);
+
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Nouveau Mise a Jour dans la base de données!");
+            alert.setHeaderText(null);
+            alert.setContentText("Promotion Ajoutée");
+            alert.showAndWait();
+            //refresh
+            Rebelottee();
+        }
+    }
+
+    @FXML
+    private void modifierPromotion(ActionEvent event) throws SQLException {
+   //ajout
+        String id = (String) tf_modif_id_promotion.getText();
+        String libelle = tf_modif_libelle_promotion.getText();
+        String PoucentageReduction = tf_modif_PoucentageReduction_promotion.getText();
+        String id_evenement = tf_modif_id_evenement_promotion.getText();
+      
+
+        Promotion p = new Promotion(parseInt(id), libelle,  parseInt(PoucentageReduction), parseInt(id_evenement));
+        ServicePromotion es = new ServicePromotion();
+
+        //controle de saisie
+        boolean cond = true;
+        if (tf_modif_libelle_promotion.getText().isEmpty() | tf_modif_PoucentageReduction_promotion.getText().isEmpty()) {
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setTitle("Erreur");
+            alert.setHeaderText(null);
+            alert.setContentText("Vérifiez vos Champs!");
+            alert.showAndWait();
+            cond = false;
+        }
+        if (cond == true) {
+            es.modifierPromotion(p);
+            //alerte de modification
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Nouveau Mise a Jour dans la base de données!");
+            alert.setHeaderText(null);
+            alert.setContentText("Promotion Modifiée");
+            alert.showAndWait();
+            //refresh
+            Rebelottee();
+        }
+    }
+
+    
+    @FXML
+    private void supprimerPromotion(ActionEvent event) throws SQLException {
+        //suppression
+
+        ServicePromotion tt = new ServicePromotion();
+        tt.supprimerPromotion(table_promotion.getSelectionModel().getSelectedItem().getId());
+        //alerte de suppression
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Nouveau Mise a Jour dans la base de données!");
+        alert.setHeaderText(null);
+        alert.setContentText("Promotion Supprimée");
+        alert.showAndWait();
+        //refresh
+        Rebelottee();
+    }
+    
+    
+    @FXML
+    private void mouseClickedd(MouseEvent event) {
+    Promotion selected = table_promotion.getSelectionModel().getSelectedItem();
+     tf_modif_id_promotion.setText(selected.getId() + "");
+    tf_modif_libelle_promotion.setText(selected.getLibelle());
+    tf_modif_PoucentageReduction_promotion.setText(selected.getPoucentageReduction()+ "");
+    tf_modif_id_evenement_promotion.setText(selected.getId_evenement() + "");
+    
+    }
+    
+    
+    @FXML
+    private void filterFieldActionn(KeyEvent event) throws SQLException {
+        
+        ServicePromotion SE = new ServicePromotion();
+        List<Promotion> promotions = SE.afficherPromotions();
+        List<Promotion> filteredpromotions = new ArrayList<>();
+        if (!filterField.getText().isEmpty()) {
+            filteredpromotions = promotions.stream()
+                    
+                    .filter(p -> p.getLibelle().startsWith(filterField.getText()))
+                
+                    .collect(Collectors.toList());
+        } else {
+            filteredpromotions = promotions;
+        }
+       observableList.clear();
+
+        col_id_entt.setCellValueFactory(new PropertyValueFactory<Promotion, Integer>("id"));
+        col_libelle_entt.setCellValueFactory(new PropertyValueFactory<Promotion, String>("libelle"));
+        col_PoucentageReduction_ent.setCellValueFactory(new PropertyValueFactory<Promotion, Integer>("PoucentageReduction"));
+        col_id_evenement_ent.setCellValueFactory(new PropertyValueFactory<Promotion, Integer>("id_evenement"));
+      
+       observableList = FXCollections.observableArrayList(filteredpromotions);
+       table_promotion.setItems(observableList);
+
+    }
+
+    @FXML
+    private void pageEvenements(ActionEvent event) {
+           try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Evenement.fxml"));
+            Parent root = loader.load();
+            EvenementController ec = loader.getController();
+            page_evenements.getScene().setRoot(root);
+            
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+    }
+
+   
+}
